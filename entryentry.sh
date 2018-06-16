721f433083e53a25ecf3f9a209c1dd6dab2e699f
jaskon139 <jaskon139@gmail.com>
2018-06-16 21:07:05 +0800

    Update entryentry.sh

--- entryentry.sh
+++ entryentry.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-qemu-system-x86_64 -nographic -net nic,vlan=0 -net user -m 128 -hda /ssh_and_ss/tc/tinycore.img < /dev/null &
+qemu-system-x86_64 -nographic -net nic,vlan=0 -net user -m 128 -hda /app/ssh_and_ss/tc/tinycore.img < /dev/null &
 
-cd /bktty && npm install && node app.js -p 3000
+cd /app/bin && npm install && node app.js -p 3000
