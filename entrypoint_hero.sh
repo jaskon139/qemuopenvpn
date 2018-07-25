70cad09abe6f7cab28ccafb59c67864399f5e3e0
jaskon139 <jaskon139@gmail.com>
2018-07-25 09:27:26 +0800

    Update entrypoint_hero.sh

--- entrypoint_hero.sh
+++ entrypoint_hero.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+cd /app/ssh_and_ss
 resultip=$(ifconfig eth0 |grep "inet "| cut -f 2 -d "t"|cut -f 1 -d "n" )
 pkill ss-server
 pkill server_linux_amd64
 pkill kcptunserver
 echo "---------------------------------------------"
 echo $resultip
-ss-server -c ./ss-configcodeing.json &
+./shadowsocks-server-linux64-1.1.5 -c ./ss-configcodeing.json &
 ./server_linux_amd64 -t 127.0.0.1:8388 -l :3824 --mode fast2& 
 ./kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 
 cd /opt/wetty && node /opt/wetty/app.js -p 3000
