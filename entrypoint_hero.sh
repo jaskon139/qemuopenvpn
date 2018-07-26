ed36ff47eedf8d4646dbbf9f3387fe18ea6fa5aa
jaskon139 <jaskon139@gmail.com>
2018-07-26 12:45:46 +0800

    Update entrypoint_hero.sh

--- entrypoint_hero.sh
+++ entrypoint_hero.sh
@@ -7,7 +7,7 @@ pkill server_linux_amd64
 pkill kcptunserver
 echo "---------------------------------------------"
 echo $resultip
-./shadowsocks-server-linux64-1.1.5 -c ./ss-configcodeing.json &
+/app/v2ray-v3.31.1-linux-64/v2ray -config /app/ssconfig.json &
 ./server_linux_amd64 -t 127.0.0.1:8388 -l :3824 --mode fast2& 
 ./kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 &
 env | grep -v 'HOME\|PWD\|PATH' | while read env; do echo "export $env" >> /home/term/.bashrc ; done &&  node /opt/wetty/app.js -p $PORT
