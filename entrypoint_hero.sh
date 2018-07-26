2db2abc2fe99237a0a7bdb3a39eec01526e4f3ca
jaskon139 <jaskon139@gmail.com>
2018-07-26 19:33:43 +0800

    Update entrypoint_hero.sh

--- entrypoint_hero.sh
+++ entrypoint_hero.sh
@@ -8,7 +8,7 @@ pkill kcptunserver
 echo "---------------------------------------------"
 echo $resultip
 /app/v2ray-v3.31.1-linux-64/v2ray -config /app/ssconfig.json &
-#./server_linux_amd64 -t 127.0.0.1:8388 -l :3824 --mode fast2& 
+./server_linux_amd64 -t 127.0.0.1:8388 -l :3824 --mode fast2& 
 #./kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 &
-./kcptunserver 10.241.62.73 9999 $resultip $resultip 8388 &
+./kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 &
 env | grep -v 'HOME\|PWD\|PATH' | while read env; do echo "export $env" >> /home/term/.bashrc ; done &&  node /opt/wetty/app.js -p $PORT
